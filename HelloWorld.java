c47c7f44ec3fab20386b8f89896d1c8e292797f1
DakotaCromwell <dcromwell31415@gmail.com>
2019-08-28 14:33:25 -0500

    Homework Assigment 1

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+//Dakota Cromwell
+//Sk221
+package com.mycompany.homeworkone;
+
+public class HelloWorld {
+    public static void main (String[] args){
+    System.out.println("Hello World !");
+    }
+}
